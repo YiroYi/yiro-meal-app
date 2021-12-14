f9b411cd8619175fd35a02cb5af9f3ac720ba5eb
YiroYi <yiroyi@live.com>
2021-12-13 21:05:02 -0600

    Implementing Searchbar

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>Search</Text>
+          <Searchbar />
         </View>
         <View style={styles.list}>
-          <Text>Yiro Yi</Text>
+          <Text>Yiro YiY</Text>
         </View>
         <ExpoStatusBar style="auto" />
       </SafeAreaView>

c4953bf77ef05169c5a71543e60876a56923b9c7
YiroYi <yiroyi@live.com>
2021-12-18 19:28:10 -0600

    Adding Context Call

--- src/services/restaurants.context.js
+++ src/services/restaurants.context.js
@@ -1,11 +1,42 @@
 import React, { useState, createContext, useEffect, useMemo } from "react";
-import { restaurantsRequest, restaurantTransform } from "./restaurants.service";
+import {
+  restaurantsRequest,
+  restaurantsTransform
+} from "./restaurants.service";
 
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const retrieveRestaurants = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      restaurantsRequest()
+        .then(restaurantsTransform)
+        .then((restaurantsResult) => {
+          console.log(restaurantsResult);
+          setIsLoading(false);
+          setRestaurants(restaurantsResult);
+        })
+        .catch((err) => setError(err));
+    }, 2000);
+  };
+
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
+
   return (
-    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+    <RestaurantsContext.Provider
+      value={{
+        restaurants,
+        isLoading,
+        error,
+      }}
+    >
       {children}
     </RestaurantsContext.Provider>
   );

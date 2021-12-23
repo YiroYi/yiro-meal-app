0f893a35c968447b633e5050f5292829571676a6
YiroYi <yiroyi@live.com>
2021-12-22 21:21:22 -0600

    Adding Map View

---
+++ src/features/map/screens/map.screen.js
@@ -0,0 +1,18 @@
+import React, { useContext, useState, useEffect } from "react";
+import MapView from "react-native-maps";
+import styled from "styled-components/native";
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const MapScreen = () => {
+  return (
+    <>
+      <Search />
+      <Map />
+    </>
+  );
+};

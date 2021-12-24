6cf279b54a97c2761f5929721b41a5403eb9c916
YiroYi <yiroyi@live.com>
2021-12-23 23:31:42 -0600

    Added Async Cache

--- src/components/favourites/favourites-bar.component.js
+++ src/components/favourites/favourites-bar.component.js
@@ -3,20 +3,29 @@ import { ScrollView, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { CompactRestaurantInfo } from "../restaurant/compact-info.component";
 import { Spacer } from "../spacer/spacer.component";
+import { Text } from "../typography/text.component";
 
 const FavouritesWrapper = styled.View`
   padding: 10px;
 `;
 
 export const FavouritesBar = ({ favourites, onNavigate }) => {
+  if (!favourites.length) {
+    return null;
+  }
+
   return (
     <FavouritesWrapper>
+      <Spacer variant="left.large">
+        <Text variant="caption">Favourites</Text>
+      </Spacer>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {favourites.map((restaurant) => {
           const key = restaurant.name;
           return (
             <Spacer key={key} position="left" size="medium">
-              <TouchableOpacity onPress={() =>
+              <TouchableOpacity
+                onPress={() =>
                   onNavigate("RestaurantDetail", {
                     restaurant,
                   })
